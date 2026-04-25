4314c8bcbde21fff9ca5d604adc855d62f846f4c
febriansyah-prog <febriansyah@example.com>
2026-04-25 22:35:59 +0700

    chore: update metadata for elegant link sharing and Unifa promotion

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,8 +14,13 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Unmute By Unifers",
-  description: "Sistem Booking Kunjungan Sekolah",
+  title: "Unmute By Unifers | Universitas Fajar",
+  description: "Program Unmute by Unifers. Sebuah inisiatif dan dedikasi pengabdian masyarakat dari sivitas akademika Universitas Fajar (Unifa) untuk mencetak generasi unggul Sulawesi Selatan melalui eskalasi keterampilan abad 21.",
+  openGraph: {
+    title: "Unmute By Unifers | Universitas Fajar",
+    description: "Program Unmute by Unifers. Sebuah inisiatif dan dedikasi pengabdian masyarakat dari sivitas akademika Universitas Fajar (Unifa) untuk mencetak generasi unggul Sulawesi Selatan melalui eskalasi keterampilan abad 21.",
+    siteName: "Unmute By Unifers",
+  }
 };
 
 export default function RootLayout({
